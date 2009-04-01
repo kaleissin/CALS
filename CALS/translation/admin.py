dd341ae9163fb306ed734c3e8d85db4677fed4cf
kaleissin <kaleissin@gmail.com>
2009-04-01 13:59:34 +0000

    Better admin

--- CALS/translation/admin.py
+++ CALS/translation/admin.py
@@ -5,9 +5,15 @@ from translation.models import Translation, TranslationExercise, TranslationExer
 class TranslationAdmin(admin.ModelAdmin):
     model = Translation
     ordering = ('exercise', 'translator', 'translation')
-    list_display = ('exercise', 'translation', 'translator')
+    list_display = ('translation', 'exercise', 'language', 'translator')
+    list_filter = ('exercise',)
 admin.site.register(Translation, TranslationAdmin)
 
-admin.site.register(TranslationExercise)
+class TranslationExerciseAdmin(admin.ModelAdmin):
+    model = TranslationExercise
+    ordering = ('category', 'name')
+    list_display = ('name', 'category')
+    list_filter = ('category',)
+admin.site.register(TranslationExercise, TranslationExerciseAdmin)
 admin.site.register(TranslationExerciseCategory)
 
