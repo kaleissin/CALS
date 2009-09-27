e672f53b91b2b4e3c932ed1f03d7f08de49b6c7f
kaleissin <kaleissin@gmail.com>
2009-09-27 11:21:52 +0000

    Better errorhanding + start of tests

--- CALS/cals/rst.py
+++ CALS/cals/rst.py
@@ -1,9 +1,12 @@
+
+# -*- coding: utf8 -*-
+
 from docutils.parsers import rst
 from docutils.parsers.rst import directives
 from docutils import nodes
 from docutils.writers import html4css1
 
-from leipzig import InterlinearText
+from interlinears.leipzig import InterlinearText, InterlinearError
 
 class CALSHTMLWriter(html4css1.Writer):
     def __init__(self):
@@ -21,15 +24,12 @@ class CALSHTMLTranslator(html4css1.HTMLTranslator):
         self.body.append(self.starttag(node, 'pre', CLASS='literal-block plaintext'))
 
     def visit_interlinear(self, node):
-        self.body.append(self.starttag(node, 'div'))
         il = InterlinearText()
         text = il.do_text(node.astext())
         self.body.append(text)
         node.children = []
 
     def depart_interlinear(self, node):
-        self.body.append('</div>') #self.endtag(node, 'div'))
-        self.body.append('<p></p>\n')
         pass
 
 class InterlinearDirective(rst.Directive):
@@ -40,47 +40,46 @@ class InterlinearDirective(rst.Directive):
 
     def run(self):
         self.assert_has_content()
-        #il = InterlinearText()
-        #il.add_lines(self.content)
         text = '\n'.join(self.content)
+        try:
+            il = InterlinearText()
+            html = il.do_text(text)
+        except InterlinearError, e:
+            raise self.error(e.args[0])
         text_nodes, messages = self.state.inline_text(text, self.lineno)
         node = self.interlinear(text, '', *text_nodes)
         node.line = self.content_offset + 1
         return [node] + messages
-
-# template tag
-# def restructuredtext(value):
-#    directives.register_directive('interlinear', InterlinearDirective)
-#     try:
-#         from docutils.core import publish_parts
-#     except ImportError:
-#         if settings.DEBUG:
-#             raise template.TemplateSyntaxError, "Error in {% restructuredtext %} filter: The Python docutils library isn't installed."
-#         return force_unicode(value)
-#     else:
-#         docutils_settings = getattr(settings, "RESTRUCTUREDTEXT_FILTER_SETTINGS", {})
-#         parts = publish_parts(source=smart_str(value), writer_name="html4css1", settings_overrides=docutils_settings)
-#         return mark_safe(force_unicode(parts["fragment"]))
-# restructuredtext.is_safe = True
-
-        
 
 if __name__ == '__main__':
     from docutils.core import publish_parts
 
     from pprint import pprint
 
-    teststring = """
+    il = InterlinearText()
+
+    directives.register_directive('interlinear', InterlinearDirective)
+
+    def test(text):
+        parts = publish_parts(source=text, writer=CALSHTMLWriter())
+        return parts['fragment']
+
+    goodteststring = """
 .. interlinear::
-    x
-    z
+    x a
+    z b
 
     y
     0
-
+    "foo ba"
 """
-    directives.register_directive('interlinear', InterlinearDirective)
 
-    parts = publish_parts(source=teststring, writer=CALSHTMLWriter())
+    badteststring = """
+.. interlinear::
+    x a
+    z
+"""
 
-    print parts['fragment']
+    print test(goodteststring)
+    print test(badteststring)
+
