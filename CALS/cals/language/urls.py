ba2db59a1a85217a5aaa7f2305c215e9098cdd81
kaleissin <kaleissin@gmail.com>
2009-09-27 16:35:58 +0000

    Tag-urls must be checked before lang-urls.

--- CALS/cals/language/urls.py
+++ CALS/cals/language/urls.py
@@ -5,6 +5,31 @@ from tagging.models import Tag
 
 from cals.models import Language, Feature, FeatureValue, Category, Profile
 
+## tagging
+tags_language_dict = {
+        'queryset_or_model': Language,
+        'template_name': 'cals/language_tag_list.html',
+#        'extra_context': { 'me': 'language' },
+}
+
+taglist_language_dict = {
+        'queryset': Tag.objects.all(), #usage_for_model(Language, counts=True),
+#        'template_name': 'tagging/tag_list.html',
+        'extra_context': { 'me': 'language' },
+}
+
+urlpatterns = patterns('tagging.views',
+        (r'^tag/(?P<tag>[- \w\d]+)/$', 'tagged_object_list', tags_language_dict),
+)
+
+urlpatterns += patterns('django.views.generic',
+        (r'^tag/$',                        'list_detail.object_list', taglist_language_dict),
+# #        (r'^latest/$', 'date_based.archive_index', language_by_date),
+# #        (r'^(?P<year>\d{4})/$',            'date_based.archive_year', language_by_year),
+        #(r'^(?P<year>\d{4})/w(?P<week>[a-z]{3})/$', 'date_based.archive_week', language_by_week),
+)
+
+# language
 # language_detail_dict = {
 #         'queryset': Language.objects.all().select_related(),
 #         'extra_context': { 'me': 'language' },
@@ -23,8 +48,7 @@ language_by_date = {
         'extra_context': { 'me': 'language' },
         }
 
-# language
-urlpatterns = patterns('cals.views',
+urlpatterns += patterns('cals.views',
         (r'^([?](?P<action>[a-z]+))?$', 'list_languages'),
         (r'^jrklist/$',                             'language_jrklist'),
         (r'^p(?P<page>[0-9]+)/$',          'language_list'),
@@ -34,32 +58,10 @@ urlpatterns = patterns('cals.views',
         (r'^(?P<lang>[-\w]+)/feature/(?P<object_id>[0-9]+)/$', 'show_languagefeature'), 
         (r'^(?P<lang>[-\w]+)/feature/(?P<object_id>[0-9]+)/change$', 'describe_languagefeature'), 
         (r'^(?P<lang>[-\w]+)/feature/(?P<object_id>[0-9]+)/use$', 'revert_languagefeature_description'), 
+        (r'^(?P<lang>[-\w]+)/feature/(?P<object_id>[0-9]+)/history/delete$', 'remove_languagefeature_description_version'), 
         (r'^(?P<lang>[-\w]+)/feature/(?P<object_id>[0-9]+)/history/$', 'show_languagefeature_history'), 
         (r'^(?P<lang>[-\w]+)/feature/(?P<object_id>[0-9]+)/history/compare$', 'compare_languagefeature_history'), 
         (r'^(?P<lang>[-\w]+)/change$',     'change_language'),
         (r'^(?P<slugs>[-+\w]+)/(?P<opt>[^/]*?)/?$',      'compare_language'), 
 )
 
-## tagging
-tags_language_dict = {
-        'queryset_or_model': Language,
-        'template_name': 'cals/language_tag_list.html',
-#        'extra_context': { 'me': 'language' },
-}
-
-taglist_language_dict = {
-        'queryset': Tag.objects.all(), #usage_for_model(Language, counts=True),
-#        'template_name': 'tagging/tag_list.html',
-        'extra_context': { 'me': 'language' },
-}
-
-urlpatterns += patterns('tagging.views',
-        (r'^tag/(?P<tag>[- \w\d]+)/$', 'tagged_object_list', tags_language_dict),
-)
-
-urlpatterns += patterns('django.views.generic',
-        (r'^tag/$',                        'list_detail.object_list', taglist_language_dict),
-# #        (r'^latest/$', 'date_based.archive_index', language_by_date),
-# #        (r'^(?P<year>\d{4})/$',            'date_based.archive_year', language_by_year),
-        #(r'^(?P<year>\d{4})/w(?P<week>[a-z]{3})/$', 'date_based.archive_week', language_by_week),
-)
