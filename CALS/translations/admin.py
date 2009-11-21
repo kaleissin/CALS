4d5343aa714c57ee620d91970a6aac231987e6cb
kaleissin <kaleissin@gmail.com>
2009-11-21 15:38:53 +0000

    Big refactor: Translation-renaming part 1

---
+++ CALS/translations/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+
+from translations.models import Translation, TranslationExercise, TranslationExerciseCategory
+
+class TranslationAdmin(admin.ModelAdmin):
+    model = Translation
+    ordering = ('exercise', 'translator', 'translation')
+    list_display = ('translation', 'exercise', 'language', 'translator')
+    list_filter = ('exercise',)
+admin.site.register(Translation, TranslationAdmin)
+
+class TranslationExerciseAdmin(admin.ModelAdmin):
+    model = TranslationExercise
+    ordering = ('category', 'name')
+    list_display = ('name', 'category')
+    list_filter = ('category',)
+admin.site.register(TranslationExercise, TranslationExerciseAdmin)
+admin.site.register(TranslationExerciseCategory)
+
---
+++ CALS/translations/signalhandlers.py
@@ -0,0 +1,19 @@
+from nano.blog import add_entry_to_blog
+from nano.badge import add_badge
+#from nano.badge.models import Badge
+
+def new_translation(sender, **kwargs):
+    "Signal handler for Translation.post_save"
+    new = kwargs[u'created']
+    trans = kwargs[u'instance']
+
+    if new:
+#         # Add relevant badge to conlanger
+#         badge = Badge.objects.get(name='Translator')
+#         add_badge(badge, trans.translator)
+
+        # Blog
+        if trans.exercise.id != 1:
+            title = u'New translation of "%s" into %s by %s' % (trans.exercise.name, trans.language, trans.translator.get_profile().display_name)
+            add_entry_to_blog(trans, title, 'translations/new_trans_blogentry.html', date_field='added')
+
