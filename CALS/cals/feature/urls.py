8cd6f1cfa47354281ed2d6de607598d3906f4653
kaleissin <kaleissin@gmail.com>
2010-01-02 00:15:07 +0000

    More manageable url-regexps

--- CALS/cals/feature/urls.py
+++ CALS/cals/feature/urls.py
@@ -6,6 +6,9 @@ from voting.views import vote_on_object
 
 from cals.models import Feature, FeatureValue, Category
 
+FEATURE_RE = r'(?P<object_id>[0-9]+)/'
+FEATURE_HISTORY_RE = r'^' + FEATURE_RE + r'history/'
+
 feature_list_dict = {
         'queryset': Category.active_objects.filter(feature__active=True).distinct().order_by('id'),
         'template_name': 'cals/feature_list.html', 
@@ -25,12 +28,12 @@ urlpatterns = patterns('django.views.generic',
 )
 
 urlpatterns += patterns('cals.views',
-        (r'^(?P<object_id>[0-9]+)/$',           'show_feature'),
-        (r'^(?P<objects>[+0-9]+)/$',            'compare_feature'), 
-        (r'^(?P<object_id>[0-9]+)/change$',     'change_feature_description'),
-        (r'^(?P<object_id>[0-9]+)/use$',        'revert_feature_description'),
-        (r'^(?P<object_id>[0-9]+)/history/$',   'show_feature_history'),
-        (r'^(?P<object_id>[0-9]+)/history/compare$', 'compare_feature_history'),
+        (r'^'+FEATURE_RE+r'$',             'show_feature'),
+        (r'^(?P<objects>[+0-9]+)/$',       'compare_feature'), 
+        (r'^'+FEATURE_RE+r'change$',       'change_feature_description'),
+        (r'^'+FEATURE_RE+r'use$',          'revert_feature_description'),
+        (FEATURE_HISTORY_RE+r'$',          'show_feature_history'),
+        (FEATURE_HISTORY_RE+r'compare$',   'compare_feature_history'),
 #         (r'^suggested/new$',                  'add_feature'),
 #         (r'^suggested/(?P<object_id>[0-9]+)/$', 'show_suggested_feature'),
 #         (r'^suggested/(?P<object_id>[0-9]+)/change$', 'change_feature'),
