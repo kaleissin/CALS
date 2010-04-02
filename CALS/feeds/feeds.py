b2f5704a851b6baf3d22823a870a17c17a9b92b1
kaleissin <kaleissin@gmail.com>
2010-04-02 10:35:08 +0000

    Current feeds

--- CALS/feeds/feeds.py
+++ CALS/feeds/feeds.py
@@ -1,18 +1,29 @@
+from datetime import datetime
+
+from django.template.defaultfilters import slugify
+
 import djatom as atom
 from nano.blog.models import Entry
 from cals.models import Language, Profile
-from translations.models import TranslationExercise
+from translations.models import TranslationExercise, Translation
+from nano.comments.models import Comment
 from django.template.loader import render_to_string
 
 STANDARD_AUTHORS = ({'name': 'admin'},)
 
 class AbstractFeed(atom.Feed):
+    _domain = 'cals.conlang.org'
     feed_icon = "http://media.aldebaaran.uninett.no/CALS/img/favicon.ico"
     feed_authors = STANDARD_AUTHORS
 
+    def _make_date(self, date):
+        return date.strftime('%Y-%m-%dT%H:%M:%S')
+
 class AllFeed(AbstractFeed):
     feed_title = 'CALS news'
-    feed_id = 'http://cals.conlang.org/feeds/all/'
+    
+    def feed_id(self):
+            return 'http://%s/feeds/all/' % self._domain
 
     def items(self):
         return Entry.objects.order_by('-pub_date')
@@ -29,16 +40,46 @@ class AllFeed(AbstractFeed):
     def item_updated(self, item):
         return item.pub_date
 
+class RecentCommentsFeed(AbstractFeed):
+    feed_title = "CALS: recent comments"
+    
+    def feed_id(self):
+            return 'http://%s/feeds/comments/' % self._domain
+
+    _item_template = 'feeds/comments_description.html'
+
+    def items(self):
+        return Comment.objects.order_by('-added')[:15]
+
+    def item_id(self, item):
+        return 'tag:%s,%s/recent/%s-%s' % (self._domain, self._make_date(item.added), slugify(item.content_object), item.user.id)
+
+    def item_title(self, item):
+        return 'New comment by %s on %s' % (item.user.get_profile().display_name, item.content_object)
+
+    def item_content(self, item):
+        d = {'obj': item}
+        return {"type": "html",}, render_to_string(self._item_template, d)
+
+    def item_authors(self, item):
+        return ({'name': unicode(item.user)},)
+
+    def item_updated(self, item):
+        return item.added
+
+    def item_published(self, item):
+        return item.added
+
 class UpdatedLanguagesFeed(AbstractFeed):
     feed_title = "CALS: recently modified languages"
-    feed_id = 'http://cals.conlang.org/feeds/languages/'
-
-    #description = "Recently modified languages at CALS"
+    
+    def feed_id(self):
+            return 'http://%s/feeds/languages/updated/' % self._domain
 
     _item_template = 'feeds/languages_description.html'
 
     def items(self):
-        return Language.objects.exclude(slug__startswith='testarossa').order_by('-last_modified')[:15]
+        return Language.objects.conlangs().exclude(slug__startswith='testarossa').order_by('-last_modified')[:15]
 
     def item_id(self, item):
         return '%s-%s' % (item.slug, item.last_modified)
@@ -61,14 +102,14 @@ class UpdatedLanguagesFeed(AbstractFeed):
 
 class NewestLanguagesFeed(AbstractFeed):
     feed_title = "CALS: recently added languages"
-    feed_id = 'http://cals.conlang.org/feeds/languages/'
+    
+    def feed_id(self):
+            return 'http://%s/feeds/languages/new/' % self._domain
 
     _item_template = 'feeds/languages_newest_description.html'
 
-    #description = "Recently added languages at CALS"
-
     def items(self):
-        return Language.objects.exclude(slug__startswith='testarossa').order_by('-created')[:15]
+        return Language.objects.conlangs().exclude(slug__startswith='testarossa').order_by('-created')[:15]
 
     def item_id(self, item):
         return '%s-created-%s' % (item.slug, item.created)
@@ -91,10 +132,10 @@ class NewestLanguagesFeed(AbstractFeed):
 
 class AllPeopleFeed(AbstractFeed):
     feed_title = "CALS: all people"
-    feed_id = "http://cals.conlang.org/feeds/people/"
-    #title_template = 'feeds/people_title_all.html'
+    
+    def feed_id(self):
+            return 'http://%s/feeds/people/changed/' % self._domain
 
-    #description = "The people of CALS"
     _item_template = 'feeds/people_description_all.html'
 
     def items(self):
@@ -105,7 +146,6 @@ class AllPeopleFeed(AbstractFeed):
 
     def item_title(self, item):
         return item.display_name
-        #return 'New conlanger: %s' % item.display_name
 
     def item_content(self, item):
         d = {'obj': item.user}
@@ -122,10 +162,10 @@ class AllPeopleFeed(AbstractFeed):
 
 class RecentlyJoinedFeed(AbstractFeed):
     feed_title = "CALS: recently joined people"
-    feed_id = "http://cals.conlang.org/feeds/people/"
-    #title_template = 'feeds/people_title_all.html'
+    
+    def feed_id(self):
+            return 'http://%s/feeds/people/new/' % self._domain
 
-#     description = "The people that most recently joined CALS"
     _item_template = 'feeds/people_description.html'
 
     def items(self):
@@ -152,10 +192,10 @@ class RecentlyJoinedFeed(AbstractFeed):
 
 class NewTranslationExerciseFeed(AbstractFeed):
     feed_title = "CALS: new translation exercises"
-    feed_id = "http://cals.conlang.org/feeds/translations/"
-    #title_template = 'feeds/people_title_all.html'
+    
+    def feed_id(self):
+            return 'http://%s/feeds/translations/exercises' % self._domain
 
-#     description = "The people that most recently joined CALS"
     _item_template = 'feeds/transex_description.html'
 
     def items(self):
@@ -173,6 +213,36 @@ class NewTranslationExerciseFeed(AbstractFeed):
 
     def item_authors(self, item):
         return ({'name': unicode(item.added_by)},)
+
+    def item_updated(self, item):
+        return item.added
+
+    def item_published(self, item):
+        return item.added
+
+class NewTranslationFeed(AbstractFeed):
+    feed_title = "CALS: new translation"
+    
+    def feed_id(self):
+            return 'http://%s/feeds/translations/new' % self._domain
+
+    _item_template = 'feeds/trans_description.html'
+
+    def items(self):
+        return Translation.objects.order_by('-added')[:15]
+
+    def item_id(self, item):
+        return '%s/%s/%s' % (item.exercise.slug, item.language, item.translator)
+
+    def item_title(self, item):
+        return '%s translated "%s" into %s' % (item.translator, item.exercise.name, item.language)
+
+    def item_content(self, item):
+        d = {'obj': item}
+        return {"type": "html",}, render_to_string(self._item_template, d)
+
+    def item_authors(self, item):
+        return ({'name': unicode(item.translator)},)
 
     def item_updated(self, item):
         return item.added
