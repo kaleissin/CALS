fc7c9083e66cc897b4a20d4ff6d8a410542c4cfe
kaleissin <kaleissin@gmail.com>
2010-05-13 16:34:00 +0000

    IDs now RFC 4151-compliant

--- CALS/feeds/feeds.py
+++ CALS/feeds/feeds.py
@@ -13,23 +13,40 @@ STANDARD_AUTHORS = ({'name': 'admin'},)
 
 class AbstractFeed(atom.Feed):
     _domain = 'cals.conlang.org'
+    category = ''
     feed_icon = "http://media.aldebaaran.uninett.no/CALS/img/favicon.ico"
     feed_authors = STANDARD_AUTHORS
 
-    def _make_date(self, date):
+    def _make_datetime(self, date):
         return date.strftime('%Y-%m-%dT%H:%M:%S')
+
+    def _make_datetimeid(self, date):
+        return date.strftime('%Y%m%d%H%M%S')
+
+    def _make_date(self, date):
+        return date.strftime('%Y-%m-%d')
+
+    def feed_id(self):
+        return 'http://%s/feeds/%s/' % (self._domain, self.category)
+
+    def base_id(self, item):
+        category = self.category
+        if category:
+            category = category + '/'
+        return 'tag:%s,%s:%s' % (self._domain, 
+                self._make_date(self.item_updated(item)), 
+                category)
+
 
 class AllFeed(AbstractFeed):
     feed_title = 'CALS news'
+    category = 'all'
     
-    def feed_id(self):
-            return 'http://%s/feeds/all/' % self._domain
-
     def items(self):
         return Entry.objects.order_by('-pub_date')
 
     def item_id(self, item):
-        return str(item.id)
+        return self.base_id(item) + str(item.id)
 
     def item_title(self, item):
         return item.headline
@@ -42,17 +59,15 @@ class AllFeed(AbstractFeed):
 
 class RecentCommentsFeed(AbstractFeed):
     feed_title = "CALS: recent comments"
+    category = 'comments/recent'
     
-    def feed_id(self):
-            return 'http://%s/feeds/comments/' % self._domain
-
     _item_template = 'feeds/comments_description.html'
 
     def items(self):
         return Comment.objects.order_by('-added')[:15]
 
     def item_id(self, item):
-        return 'tag:%s,%s/recent/%s-%s' % (self._domain, self._make_date(item.added), slugify(item.content_object), item.user.id)
+        return self.base_id(item) + '/%s-%s' % (slugify(item.content_object), item.user.id)
 
     def item_title(self, item):
         return 'New comment by %s on %s' % (item.user.get_profile().display_name, item.content_object)
@@ -72,17 +87,17 @@ class RecentCommentsFeed(AbstractFeed):
 
 class UpdatedLanguagesFeed(AbstractFeed):
     feed_title = "CALS: recently modified languages"
+    category = 'languages/updated'
     
-    def feed_id(self):
-            return 'http://%s/feeds/languages/updated/' % self._domain
-
     _item_template = 'feeds/languages_description.html'
 
     def items(self):
         return Language.objects.conlangs().exclude(slug__startswith='testarossa').order_by('-last_modified')[:15]
 
     def item_id(self, item):
-        return '%s-%s' % (item.slug, item.last_modified)
+        return '%s%s-%s' % (self.base_id(item), 
+                item.slug,
+                self._make_datetimeid(self.item_updated(item)))
 
     def item_title(self, item):
         return 'Changed language: %s' % item.name
@@ -102,17 +117,17 @@ class UpdatedLanguagesFeed(AbstractFeed):
 
 class NewestLanguagesFeed(AbstractFeed):
     feed_title = "CALS: recently added languages"
+    category = 'languages/new'
     
-    def feed_id(self):
-            return 'http://%s/feeds/languages/new/' % self._domain
-
     _item_template = 'feeds/languages_newest_description.html'
 
     def items(self):
         return Language.objects.conlangs().exclude(slug__startswith='testarossa').order_by('-created')[:15]
 
     def item_id(self, item):
-        return '%s-created-%s' % (item.slug, item.created)
+        return '%s%s/%s' % (self.base_id(item), 
+                        item.slug,
+                        item.id)
 
     def item_title(self, item):
         return 'New language: %s' % item.name
@@ -132,17 +147,17 @@ class NewestLanguagesFeed(AbstractFeed):
 
 class AllPeopleFeed(AbstractFeed):
     feed_title = "CALS: all people"
+    category = 'people/changed'
     
-    def feed_id(self):
-            return 'http://%s/feeds/people/changed/' % self._domain
-
     _item_template = 'feeds/people_description_all.html'
 
     def items(self):
         return Profile.objects.filter(user__is_active=True).exclude(username__startswith='countach').order_by('display_name')
 
     def item_id(self, item):
-        return item.display_name
+        return '%s%s-%s' % (self.base_id(item),
+                item.display_name,
+                self._makedatetimeid(self.item_updated(item)))
 
     def item_title(self, item):
         return item.display_name
@@ -162,17 +177,17 @@ class AllPeopleFeed(AbstractFeed):
 
 class RecentlyJoinedFeed(AbstractFeed):
     feed_title = "CALS: recently joined people"
+    category = 'people/new'
     
-    def feed_id(self):
-            return 'http://%s/feeds/people/new/' % self._domain
-
     _item_template = 'feeds/people_description.html'
 
     def items(self):
         return Profile.objects.exclude(user__is_active=False, username__startswith='countach').order_by('-user__date_joined')[:15]
 
     def item_id(self, item):
-        return item.display_name
+        return '%s%s-%s' % (self.base_id(item),
+                item.display_name,
+                self._make_datetimeid(self.item_published(item)))
 
     def item_title(self, item):
         return '%s just joined!' % item.display_name
@@ -192,17 +207,16 @@ class RecentlyJoinedFeed(AbstractFeed):
 
 class NewTranslationExerciseFeed(AbstractFeed):
     feed_title = "CALS: new translation exercises"
+    category = 'translations/exercises'
     
-    def feed_id(self):
-            return 'http://%s/feeds/translations/exercises' % self._domain
-
     _item_template = 'feeds/transex_description.html'
 
     def items(self):
         return TranslationExercise.objects.order_by('-added')[:15]
 
     def item_id(self, item):
-        return item.slug
+        return '%s%s' % (self.base_id(item),
+                item.slug)
 
     def item_title(self, item):
         return 'To translate: "%s"' % item.name
@@ -222,17 +236,17 @@ class NewTranslationExerciseFeed(AbstractFeed):
 
 class NewTranslationFeed(AbstractFeed):
     feed_title = "CALS: new translation"
+    category = 'translations/new'
     
-    def feed_id(self):
-            return 'http://%s/feeds/translations/new' % self._domain
-
     _item_template = 'feeds/trans_description.html'
 
     def items(self):
         return Translation.objects.order_by('-added')[:15]
 
     def item_id(self, item):
-        return '%s/%s/%s' % (item.exercise.slug, item.language, item.translator)
+        return self.base_id(item) + '%s/%s/%s' % (item.exercise.slug, 
+                slugify(item.language), 
+                slugify(item.translator.username))
 
     def item_title(self, item):
         return '%s translated "%s" into %s' % (item.translator, item.exercise.name, item.language)
