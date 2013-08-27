e22eb6782905fbcf1144df6a7d3d42df7add6b0e
kaleissin <kaleissin@gmail.com>
2013-08-27 21:36:30 +0200

    Unused imports, light linting

--- src/cals/language/views.py
+++ src/cals/language/views.py
@@ -1,13 +1,13 @@
 import logging
 _LOG = logging.getLogger(__name__)
 
-from django.http import HttpResponseRedirect, HttpResponseNotFound, HttpResponseForbidden
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.forms.models import modelformset_factory
 
 from nano.tools import render_page
 
-from cals.models import LanguageName, Language
+from cals.language.models import LanguageName, Language
 
 def _get_lang(*args, **kwargs):
     return get_object_or_404(Language, slug=kwargs.get('lang', None))
@@ -18,9 +18,9 @@ def change_languagenames(request, *args, **kwargs):
             language=lang).exclude(
             name=lang.name).exclude(
             name=lang.internal_name, internal=True).order_by('added')
-    LanguageNameFormset = modelformset_factory(LanguageName, can_delete=True, exclude=('language',), extra=3)
+    languagenameformset = modelformset_factory(LanguageName, can_delete=True, exclude=('language',), extra=3)
     if request.method == "POST":
-        formset = LanguageNameFormset(request.POST, request.FILES, queryset=queryset)
+        formset = languagenameformset(request.POST, request.FILES, queryset=queryset)
         if formset.is_valid():
             instances = formset.save(commit=False)
             for instance in instances:
@@ -28,8 +28,8 @@ def change_languagenames(request, *args, **kwargs):
                 instance.save()
             return HttpResponseRedirect(lang.get_absolute_url())
     else:
-        formset = LanguageNameFormset(queryset=queryset)
+        formset = languagenameformset(queryset=queryset)
         #assert False, dir(formset)
     data = {'me': 'language',
-        'formset': formset }
+            'formset': formset }
     return render_page(request, 'cals/language/name_form.html', data)
