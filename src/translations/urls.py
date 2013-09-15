b4af1a1586584408bca59ee868f3fed5a41f0a39
kaleissin <kaleissin@gmail.com>
2013-09-15 17:44:52 +0200

    Better order no longer masks view

--- src/translations/urls.py
+++ src/translations/urls.py
@@ -9,11 +9,11 @@ TRANSLATION_RE = r'^(?P<slug>'+Translation.RE+r')$'
 
 urlpatterns = patterns('translations.views',
         (r'^$',                       'list_all_translations'),
-        (LANG_RE+r'$',                'show_languagetranslations'), 
         (EXERCISE_LANG_RE+r'$',       'list_translation_for_language'), 
         (EXERCISE_LANG_RE+r'new$',    'add_languagetranslations'), 
         (EXERCISE_LANG_RE+r'change$', 'change_languagetranslations'), 
         (EXERCISE_LANG_RE+r'delete$', 'delete_languagetranslations'), 
+        (LANG_RE+r'$',                'show_languagetranslations'), 
         (TRANSLATION_RE,              'show_translation_for_language'), 
         (EXERCISE_RE+r'$',            'show_translationexercise'),
 )
