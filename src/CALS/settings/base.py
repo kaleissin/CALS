5933ecd48145c5bbd8f4426fdce1a9de88a204f6
kaleissin <kaleissin@gmail.com>
2014-09-24 21:10:40 +0200

    Never got it to work anyway

--- src/CALS/settings/base.py
+++ src/CALS/settings/base.py
@@ -140,7 +140,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
-    'social_auth.middleware.SocialAuthExceptionMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
