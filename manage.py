0433474362907657fe008df13e8bb5864bbc622a
kaleissin <kaleissin@gmail.com>
2015-02-15 13:37:15 +0100

    Use manage.py for manual stuff

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 sys.path.append('./src')
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CALS.settings.production")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CALS.settings.local")
 
     from django.core.management import execute_from_command_line
 
