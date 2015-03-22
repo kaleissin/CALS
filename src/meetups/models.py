8698ca48dd6273d7dee8eca6c6fc59403296233f
kaleissin <kaleissin@gmail.com>
2015-03-22 23:08:27 +0100

    Add Meetup.get_absolute_url

--- src/meetups/models.py
+++ src/meetups/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.core.urlresolvers import reverse_lazy
 
 from verification.generators import Registry
 
@@ -12,6 +13,10 @@ class Meetup(models.Model):
     badge = models.ForeignKey('badge.Badge')
     valid_from = models.DateField(blank=True, null=True)
     valid_until = models.DateField(blank=True, null=True)
+
+    def get_absolute_url(self):
+        return reverse_lazy('meetup-activate', kwargs={'group': self.keygroup.name})
+
 
 registry = Registry()
 registry.register('cals', CALSGenerator)
