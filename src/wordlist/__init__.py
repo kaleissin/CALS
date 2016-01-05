80c9a1b40468ec8c0f08243501ee9f7303431c07
kaleissin <kaleissin@gmail.com>
2016-01-05 20:29:32 +0100

    Appified wordlist

--- src/wordlist/__init__.py
+++ src/wordlist/__init__.py
@@ -1,5 +1,8 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
+
+default_app_config = 'wordlist.apps.WordlistConfig'
+
 import logging
 
 from django.db.models import Q
---
+++ src/wordlist/apps.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals
+from __future__ import absolute_import
+
+from django.apps import AppConfig
+
+
+class WordlistConfig(AppConfig):
+    name = 'wordlist'
+    verbose_name = 'Wordlist'
