8db1a1b78359177fc3d5b13970ac30badd138782
kaleissin <kaleissin@gmail.com>
2016-03-05 19:19:45 +0100

    Fix to #41

--- src/cals/language/views.py
+++ src/cals/language/views.py
@@ -24,24 +24,26 @@ def _get_lang(all=False, *args, **kwargs):
         return get_object_or_404(Language.all_langs, slug=kwargs.get('lang', None))
     return get_object_or_404(Language, slug=kwargs.get('lang', None))
 
+
 def change_languagenames(request, *args, **kwargs):
     lang = _get_lang(all=False, *args, **kwargs)
     queryset = LanguageName.objects.filter(
-            language=lang).exclude(
-            name=lang.name).exclude(
-            name=lang.internal_name, internal=True).order_by('added')
+        language=lang).exclude(
+        name=lang.name).exclude(
+        name=lang.internal_name, internal=True).order_by('added')
     languagenameformset = modelformset_factory(LanguageName, can_delete=True, exclude=('language',), extra=3)
     if request.method == "POST":
         formset = languagenameformset(request.POST, request.FILES, queryset=queryset)
         if formset.is_valid():
             instances = formset.save(commit=False)
+            for obj in formset.deleted_objects:
+                obj.delete()
             for instance in instances:
                 instance.language = lang
                 instance.save()
             return HttpResponseRedirect(lang.get_absolute_url())
     else:
         formset = languagenameformset(queryset=queryset)
-        #assert False, dir(formset)
     data = {'me': 'language',
             'formset': formset,
             'initial_names': queryset.count()}
