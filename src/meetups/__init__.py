6aebe6f05198694c8df70da674bce4ad84548429
kaleissin <kaleissin@gmail.com>
2017-04-22 17:55:52 +0200

    Appify meetups

---
+++ src/meetups/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'meetups.apps.MeetupsConfig'
---
+++ src/meetups/apps.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class MeetupsConfig(AppConfig):
+    name = "meetups"
+    verbose_name = "Meetups"
