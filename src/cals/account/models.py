9c75fb91a80b4e45e0a3497a4006a20b0174bc91
kaleissin <kaleissin@gmail.com>
2018-04-08 15:37:40 +0200

    Manually recreate a standard auth.User

--- src/cals/account/models.py
+++ src/cals/account/models.py
@@ -1,6 +1,68 @@
+from __future__ import unicode_literals
+
+from django.core import validators
+from django.core.mail import send_mail
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import UserManager
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
 
 
-class User(AbstractUser):
-    pass
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    An abstract base class implementing a fully featured User model with
+    admin-compliant permissions.
+
+    Username, password and email are required. Other fields are optional.
+    """
+    username = models.CharField(_('username'), max_length=30, unique=True,
+            help_text=_('Required. 30 characters or fewer. Letters, digits and '
+                        '@/./+/-/_ only.'),
+            validators=[
+                validators.RegexValidator(r'^[\w.@+-]+$',
+                                          _('Enter a valid username. '
+                                            'This value may contain only letters, numbers '
+                                            'and @/./+/-/_ characters.'), 'invalid'),
+            ],
+            error_messages={
+                'unique': _("A user with that username already exists."),
+    })
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    last_name = models.CharField(_('last name'), max_length=30, blank=True)
+    email = models.EmailField(_('email address'), blank=True)
+    is_staff = models.BooleanField(_('staff status'), default=False,
+        help_text=_('Designates whether the user can log into this admin site.')
+    )
+    is_active = models.BooleanField(_('active'), default=True,
+        help_text=_('Designates whether this user should be treated as '
+                    'active. Unselect this instead of deleting accounts.'))
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    class Meta:
+        swappable = 'AUTH_USER_MODEL'
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def get_full_name(self):
+        """
+        Returns the first_name plus the last_name, with a space in between.
+        """
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        "Returns the short name for the user."
+        return self.first_name
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """
+        Sends an email to this User.
+        """
+        send_mail(subject, message, from_email, [self.email], **kwargs)
